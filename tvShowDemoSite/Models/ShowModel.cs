a4dd4412183409f0957ed505f757f37b613b0440
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2016-02-11 17:10:46 +0000

    Fix spacing between property definitions.

--- tvShowDemoSite/Models/ShowModel.cs
+++ tvShowDemoSite/Models/ShowModel.cs
@@ -15,36 +15,44 @@ namespace tvShowDemoSite.Models
         /// Unique identifier for the show. 
         /// </summary>
         public string Id { get; }
+        
         /// <summary>
         /// Official title of the show, upon original release.
         /// </summary>
         public string Title { get; set; }
+        
         /// <summary>
         /// Original official release date, based on original network.
         /// </summary>
         public DateTime ReleaseDate { get; set; }
+        
         /// <summary>
         /// Original official network.
         /// </summary>
         public string Network { get; set; }
+        
         /// <summary>
         /// Genre(s). Single genre per value.
         /// </summary>
         public List<string> Genre { get; set; }
+        
         /// <summary>
         /// Officially credited creator(s). Single creator per value.
         /// </summary>
         public List<string> CreatedBy { get; set; }
+        
         /// <summary>
         /// Original official language(s).
         /// </summary>
         public List<string> Language { get; set; }
+        
         /// <summary>
         /// Total number of official seasons currently released.
         /// Does not include any sort of upcoming season(s).
         /// At least one episode must be officially released for a season to qualify.
         /// </summary>
         public int Seasons { get; set; }
+        
         /// <summary>
         /// Total number of offical episodes currently released.
         /// Does not include any sort of upcoming episode(s).
