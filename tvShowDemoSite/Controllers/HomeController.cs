83b3f9e7eb47e1a2934f7d391ba89d6e7e9ae068
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2016-02-11 20:36:02 +0000

    Add some test code for DAL functionality.

--- tvShowDemoSite/Controllers/HomeController.cs
+++ tvShowDemoSite/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tvShowDemoSite.DAL;
+using tvShowDemoSite.Models;
 
 namespace tvShowDemoSite.Controllers
 {
@@ -11,7 +13,19 @@ namespace tvShowDemoSite.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            ShowRepository repo = new ShowRepository();
+            List<ShowModel> shows = repo.Find(x => x.Title != null);
+
+            return View(shows);
+        }
+
+        public ActionResult Add(string name)
+        {
+            ShowRepository repo = new ShowRepository();
+            ShowModel show = new ShowModel();
+            show.Id = ShowRepository.GenerateId();
+            show.Title = name;
+            return View(show);
         }
     }
 }
