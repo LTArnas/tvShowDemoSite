e52323967e9b3c153d42ed92135c1b1ab6cdeb7a
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2016-02-11 17:03:44 +0000

    Basic rough implementation for Show repository. Fix Show model to have public accessiblity.

---
+++ tvShowDemoSite/DAL/ShowRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using tvShowDemoSite.Models;
+
+/* TODO: Add safety to this class' interactions/behaviours.
+Currently we will not know if something went wrong when using this class,
+unless the underlining driver throws an exception.
+
+Need to find out how MongoDB/the C# driver, handles database operation errors/results.
+C# driver docs doesn't seem to note what exceptions get thrown but,
+this might be intentional, 
+if DB operation errors/results are indicated/handled differently.
+Possible that errors are done via WriteConcern, WriteConcernError, etc.
+So, look into write concern, then add safeties here.
+*/
+
+namespace tvShowDemoSite.DAL
+{
+    /// <summary>
+    /// Repository for ShowModel.
+    /// </summary>
+    class ShowRepository : MongoDAL
+    {
+        protected IMongoCollection<ShowModel> collection;
+        
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="collectionName">Optional. Advanced use only. Name of the MongoDB collection to use.</param>
+        public ShowRepository(string collectionName = "Show") : base()
+        {
+            collection = database.GetCollection<ShowModel>(collectionName);
+        }
+
+        public void Insert(ShowModel show)
+        {
+            collection.InsertOne(show);
+        }
+        // TODO: Consider converting this function into an index accessor. Repo[string]
+        public ShowModel Get(string id)
+        {
+            return collection.AsQueryable().First(x => x.Id == id);
+        }
+
+        public List<ShowModel> Find(Expression<Func<ShowModel, bool>> predicate, int count = 10)
+        {
+            // TODO: Consider applying a max value cap for count argument.
+            return collection.AsQueryable().Where(predicate).Take(count).ToList();
+        }
+    }
+}
--- tvShowDemoSite/Models/ShowModel.cs
+++ tvShowDemoSite/Models/ShowModel.cs
@@ -9,46 +9,46 @@ namespace tvShowDemoSite.Models
     /// <summary>
     /// Holds data for a single TV show.
     /// </summary>
-    class ShowModel
+    public class ShowModel
     {
         /// <summary>
         /// Unique identifier for the show. 
         /// </summary>
-        string Id { get; }
+        public string Id { get; }
         /// <summary>
         /// Official title of the show, upon original release.
         /// </summary>
-        string Title { get; set; }
+        public string Title { get; set; }
         /// <summary>
         /// Original official release date, based on original network.
         /// </summary>
-        DateTime ReleaseDate { get; set; }
+        public DateTime ReleaseDate { get; set; }
         /// <summary>
         /// Original official network.
         /// </summary>
-        string Network { get; set; }
+        public string Network { get; set; }
         /// <summary>
         /// Genre(s). Single genre per value.
         /// </summary>
-        List<string> Genre { get; set; }
+        public List<string> Genre { get; set; }
         /// <summary>
         /// Officially credited creator(s). Single creator per value.
         /// </summary>
-        List<string> CreatedBy { get; set; }
+        public List<string> CreatedBy { get; set; }
         /// <summary>
         /// Original official language(s).
         /// </summary>
-        List<string> Language { get; set; }
+        public List<string> Language { get; set; }
         /// <summary>
         /// Total number of official seasons currently released.
         /// Does not include any sort of upcoming season(s).
         /// At least one episode must be officially released for a season to qualify.
         /// </summary>
-        int Seasons { get; set; }
+        public int Seasons { get; set; }
         /// <summary>
         /// Total number of offical episodes currently released.
         /// Does not include any sort of upcoming episode(s).
         /// </summary>
-        int Episodes { get; set; }
+        public int Episodes { get; set; }
     }
 }
