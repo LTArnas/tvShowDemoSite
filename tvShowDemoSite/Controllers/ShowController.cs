94d743818f6078f3bc17658b33fca505218458fd
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2016-02-12 21:14:36 +0000

    Add scaffolded views, controller, and Todo.txt.

---
+++ tvShowDemoSite/Controllers/ShowController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tvShowDemoSite.Controllers
+{
+    public class ShowController : Controller
+    {
+        public ActionResult Create(string title)
+        {
+            return View();
+        }
+
+        public ActionResult Read()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ tvShowDemoSite/Todo.md
@@ -0,0 +1,8 @@
+﻿# TODO! :
+
+## Check/Validate the scaffolding stuff.
+
+- Check bootstrap version used.
+- Check jQuery version used.
+- Check use of modernizer, do we need it?
+- Check generated HTML, that bootstrap is used correctly.
